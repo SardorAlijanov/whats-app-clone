7e78fe3071793f8cbd30d54a603643b2ae57becc
SardorAlijanov <s.alijanov@studentinha.uz>
2020-08-28 09:21:33 +0500

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtCsYGRGxUYUfHamVxy1Eo7o7VKE3IBek",
+  authDomain: "whats-app-clone-a5cb1.firebaseapp.com",
+  databaseURL: "https://whats-app-clone-a5cb1.firebaseio.com",
+  projectId: "whats-app-clone-a5cb1",
+  storageBucket: "whats-app-clone-a5cb1.appspot.com",
+  messagingSenderId: "901823262019",
+  appId: "1:901823262019:web:7eb301f2300d052f1513f6",
+  measurementId: "G-D0KYKW2XXL",
+};
